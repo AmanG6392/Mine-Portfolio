f8ee4f1c44ba6e0f7494d856a108afb762758d44
Aman <amanguptaa127@gmail.com>
2026-03-07 22:08:29 +0530

    just checking

--- backend/controller/emailController.js
+++ backend/controller/emailController.js
@@ -2,8 +2,9 @@ import transporter from "../configurations/mailer.js";
 
 export const sendContactEmail = async (req, res) => {
   const { name, email, message } = req.body;
-  
 
+  console.log("EMAIL_USER:", process.env.EMAIL_USER);
+  console.log("EMAIL_PASS:", process.env.EMAIL_PASS);
   // Basic validation
   if (!name || !email || !message) {
     return res.status(400).json({
@@ -14,8 +15,6 @@ export const sendContactEmail = async (req, res) => {
 
   try {
     const mailOptions = {
-        
-        
       from: `"Portfolio Contact" <${process.env.EMAIL_USER}>`,
       to: process.env.EMAIL_USER,
       replyTo: email,
@@ -35,14 +34,12 @@ User Agent: ${req.headers["user-agent"]}
       `,
     };
 
-
     await transporter.sendMail(mailOptions);
 
     res.status(200).json({
       success: true,
       message: "Message sent successfully",
     });
-
   } catch (error) {
     console.error("Email error:", error);
 
